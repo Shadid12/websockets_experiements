41b9d118e2eac0ac786cbaddb3d57f3095c87609
shadid12 <shadidhaque2014@gmail.com>
2017-07-04 02:48:06 +0000

    second script done

---
+++ convo/server.js
@@ -0,0 +1,35 @@
+const express = require("express"),
+      app     = express(),
+      http    = require("http").Server(app),
+      io  = require("socket.io")(http);
+
+const rooms = ['SOFE', 'Banaa', 'Python', 'JS']
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// socket
+
+io.on('connection', (o) => {
+    o.on('send', (data) => {
+      io.emit('sent', data); 
+    //   io.emit('user.add', o.id);
+    });
+});
+
+io.on('connection', (o) => {
+    io.emit('user.add', o.id);
+    o.on('disconnect', () => {
+        io.emit('user.remove', o.id);
+    });
+});
+
+
+// end
+
+
+http.listen(8080, function(){
+  console.log('listening on port 8080');
+});
